2e1e74ea52393ec21d04003ea8160524d27395f8
Hendrikto <hendrik.to@gmail.com>
2020-05-13 00:39:41 +0200

    Implement game stub

---
+++ pacman/game.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+class Game():
+    def __init__(self):
+        self.walls = np.array((
+            (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            (0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0),
+            (0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0),
+            (0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0),
+            (1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1),
+            (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            (0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0),
+            (0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0),
+            (0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0),
+            (0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0),
+            (0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
+        ), dtype=np.bool8)
+        self.board_size = np.array(self.walls.shape, dtype=np.int8)
+
+        self.reset()
+
+    def __str__(self):
+        shape = self.board_size + (2, 3)  # add size for frame and newlines
+
+        characters = np.chararray(shape, unicode=True)
+        characters[1:-1, 1:-2] = ' '
+        characters[:, -1] = '\n'
+
+        # frame
+        characters[[0, -1], 1:-2] = '─'
+        characters[1:-1, [0, -2]] = '│'
+        characters[0, 0] = '┌'
+        characters[0, -2] = '┐'
+        characters[-1, 0] = '└'
+        characters[-1, -2] = '┘'
+
+        # objects
+        characters[1:-1, 1:-2][self.walls] = '█'
+        characters[tuple(self.pacman + 1)] = 'P'
+
+        return ''.join(characters.flat)
+
+    def reset(self):
+        self.pacman = np.array((5, 5), dtype=np.int8)
