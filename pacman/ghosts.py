6dc691cbbe7b7752fedeebc23821c38e0da49550
Hendrikto <hendrik.to@gmail.com>
2020-05-25 21:20:11 +0200

    Make ghosts killable

--- pacman/ghosts.py
+++ pacman/ghosts.py
@@ -1,12 +1,22 @@
+import enum
+
 import numpy as np
 
 from .direction import Direction
 
 
 class Ghost():
+    class State(enum.Enum):
+        ALIVE = enum.auto()
+        DEAD = enum.auto()
+
     def __init__(self, game, position):
         self.game = game
         self.position = np.array(position, dtype=np.int8)
+        self.state = Ghost.State.ALIVE
+
+    def kill(self):
+        self.state = Ghost.State.DEAD
 
 
 class ChasingGhost(Ghost):
